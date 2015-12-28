ef5a88c46988d57f2e6c4a9fecd34422d88fce23
bigfa <jigoulee@gmail.com>
2015-12-28 13:08:04 +0800

    add typecho branch

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
-<?php get_header();?>
-    <main class="main-content">
-        <section class="blockGroup">
-            <?php if (have_posts()):
-                while (have_posts()): the_post();
-                    get_template_part('template-parts/content', get_post_format());
-                endwhile;
-            endif;?>
-        </section>
-        <div class="u-textAlignCenter postsFooterNav">
-            <div class="posts-nav">
-                <?php echo paginate_links( array(
-                    'prev_next'          => 0,
-                    'before_page_number' => '',
-                    'mid_size' => 2
-                ) );?>
-            </div>
-            </div>
-    </main>
-<?php get_footer();?>
+<?php
+/**
+ * @package Puma
+ * @author Bigfa
+ * @version 1.0.4
+ * @link http://fatesinger.com
+ */
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('header.php');
+?>
+<main class="main-content">
+    <section class="blockGroup">
+        <?php while($this->next()): ?>
+            <article class="block block--inset block--list" itemscope itemtype="http://schema.org/BlogPosting">
+                <h2 class="block-title post-featured" itemprop="headline">
+                    <a href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                </h2>
+                <div class="block-postMetaWrap u-textAlignCenter">
+                    <time><?php $this->date('Y/m/d'); ?></time>
+                </div>
+                <div class="post-content grap" itemprop="articleBody">
+                    <?php $this->content(''); ?>
+                </div>
+                <div class="block-footer">
+                    By <a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a> . In <?php $this->category(','); ?> .
+                    <div class="block-footer-inner"><?php $this->commentsNum('No relay', '1 reply', '%d replies'); ?>. </div>
+                </div>
+            </article>
+        <?php endwhile; ?>
+    </section>
+    <?php $this->pageNav('<', '>'); ?>
+</main>
+<?php $this->need('footer.php'); ?>
--- page.php
+++ page.php
@@ -1,19 +1,19 @@
-<?php get_header();?>
-    <main class="main-content">
-        <section class="section-body">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <header class="section-header u-textAlignCenter">
-                    <h2 class="grap--h2"><?php the_title();?></h2>
-                </header>
-                <div class="grap">
-                    <?php the_content();?>
-                </div>
-                <?php
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
-                ?>
-            <?php endwhile; ?>
-        </section>
-    </main>
-<?php get_footer();?>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+<main class="main-content">
+    <section class="section-body">
+        <header class="section-header u-textAlignCenter">
+            <h2 class="grap--h2"><?php $this->title() ?></h2>
+            <div class="block-postMetaWrap">
+                <time><?php $this->date('F j, Y'); ?></time>
+            </div>
+        </header>
+        <div class="grap" itemprop="articleBody">
+            <?php $this->content(); ?>
+        </div>
+    </section>
+    <?php $this->need('comments.php'); ?>
+</main><!-- end #main-->
+
+
+<?php $this->need('footer.php'); ?>
